9ef33bdfc1aa398293747d2cf7d1271491c22ffd
Marcelo Madeira <marcelo.madeira@efabrika.com.br>
2016-09-27 15:58:25 -0300

    Alterando para uso com Env Vars

--- Application/Core/Config.php
+++ Application/Core/Config.php
@@ -8,13 +8,13 @@ namespace Application\Core;
  */
 class Config
 {
-    public static $site_url = '';
-    public static $db_host = "104.236.170.225";
-    public static $db_name = "acnm";
-    public static $db_user = "acnm";
-    public static $db_pwrd = "V7ag)#%MYa";
-    public static $encryption_cost = 11;
-    public static $encryption_salt = "marcelomadeira_niguacu";
+    public static $site_url = null;
+    public static $db_host = null;
+    public static $db_name = null;
+    public static $db_user = null;
+    public static $db_pwrd = null;
+    public static $encryption_cost = null;
+    public static $encryption_salt = null;
     public static $default_controller = "home";
     public static $default_action = "index";
     public static $htaccess_param = "p";
